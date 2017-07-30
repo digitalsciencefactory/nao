522c357a7f68998dbfc029d7bbd64f45886b9e3b
sygo64 <sygo64@users.noreply.github.com>
2017-07-30 13:40:46 +0200

    Correction de bugs mineurs

---
+++ src/AppBundle/Controller/ParticiperController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class ParticiperController extends Controller
+{
+    /**
+     * @Route("/participer/espace-naturaliste", name="fn_participer_espace_nat")
+     */
+    public function espaceNatAction (Request $request)
+    {
+        /* todo:Compléter la méthode */
+        return $this->render('Participer/espace-naturaliste.html.twig');
+    }
+
+    /**
+     * @Route("/participer/envoi-observation", name="fn_participer_envoi_obs")
+     */
+    public function envoiObsAction (Request $request)
+    {
+        /* todo:Compléter la méthode */
+        return $this->render('Participer/envoi-observation.html.twig');
+    }
+
+    /**
+     * @Route("/participer/carte-des-observations", name="fn_participer_map")
+     * @Route("/participer")
+     */
+    public function mapAction (Request $request)
+    {
+        /* todo:Compléter la méthode */
+        return $this->render('Participer/carte-des-observations.html.twig');
+    }
+
+    /**
+     * @Route("/participer/nom-compte", name="fn_front_profil")
+     */
+    public function profilAction (Request $request)
+    {
+        /* todo:Compléter la méthode */
+        return $this->render('Participer/mon-compte.html.twig');
+    }
+}
