d6755350dcc8b340de24551839f397aa14bbd3c7
Simon Lhoir <simon@lhoir.me>
2017-08-09 12:45:03 +0200

    autocomplete correction des valeurs null

--- web/assets/fnat/js/autocomplete.js
+++ web/assets/fnat/js/autocomplete.js
@@ -32,12 +32,7 @@ $(function() {
 
         select: function(event, ui) {
 
-            function noNull(stringToVerif){
-                if(stringToVerif === null || stringToVerif === 'undefined'){
-                    return  '-';
-                }
-                return stringToVerif;
-            }
+
             var lbNom = ui.item.lbNom;
 
             $("input[data-id=especeId]").val(noNull(ui.item.lbNom) + ", " + noNull(ui.item.nomVern) + ", " + noNull(ui.item.nomVernEng) );
@@ -54,11 +49,18 @@ $(function() {
              item.nomVernEng + "</span></div>")
          .appendTo(ul);*/
             .append('<div class="container-fluid acItem"><div class="col-sm-12">' +
-                item.lbNom + '</div><div class="col-sm-12 name">' +
-                item.nomVern + '</div><div class="col-sm-12 name">' +
-                item.nomVernEng + '</div></div>'
+                noNull(item.lbNom)+ '</div><div class="col-sm-12 name">' +
+                    noNull(item.nomVern) + '</div><div class="col-sm-12 name">' +
+                        noNull(item.nomVernEng) + '</div></div>'
             )
             .appendTo(ul);
     };
+
+    function noNull(stringToVerif){
+        if(stringToVerif === null || stringToVerif === 'undefined'){
+            return  '-';
+        }
+        return stringToVerif;
+    }
 
 });
