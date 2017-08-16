c0536da5c9219939203f406d67622bce15d26f4f
sygo64 <sygo64@users.noreply.github.com>
2017-08-16 21:01:09 +0200

    Generation auto des points sur la carte
    Generation du fichier XML pour affichage des point observation

--- web/assets/fnat/js/googlemaps_carte_observation.js
+++ web/assets/fnat/js/googlemaps_carte_observation.js
@@ -9,21 +9,11 @@ function initMap() {
         var xml = data.responseXML;
         var markers = xml.documentElement.getElementsByTagName('marker');
         Array.prototype.forEach.call(markers, function(markerElem) {
-            var name = markerElem.getAttribute('name');
-            var address = markerElem.getAttribute('address');
+
             var point = new google.maps.LatLng(
                 parseFloat(markerElem.getAttribute('lat')),
                 parseFloat(markerElem.getAttribute('lng')));
 
-            var infowincontent = document.createElement('div');
-            var strong = document.createElement('strong');
-            strong.textContent = name
-            infowincontent.appendChild(strong);
-            infowincontent.appendChild(document.createElement('br'));
-
-            var text = document.createElement('text');
-            text.textContent = address
-            infowincontent.appendChild(text);
             var marker = new google.maps.Marker({
                 map: map,
                 position: point
