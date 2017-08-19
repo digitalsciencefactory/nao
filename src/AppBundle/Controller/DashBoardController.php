cd33a262548b0b76daa3a9584b0f7c844d1aa3bc
Simon Lhoir <simon@lhoir.me>
2017-08-19 19:36:35 +0200

    #28 modification du menu en fonction du role, modification partie espace naturaliste du menu normal

--- src/AppBundle/Controller/DashBoardController.php
+++ src/AppBundle/Controller/DashBoardController.php
@@ -3,15 +3,25 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DashBoardController extends Controller
 {
     /**
      * @Route("/dashboard", name="fn_dashboard_index")
+     * @Security("has_role('ROLE_ADMIN')")
      */
     public function indexAction(){
         return $this->render('dashboard/accueil.html.twig');
+    }
+
+    /**
+     * @Route("/dashboard/extraction-donnees", name="fn_dashboard_bdd")
+     * @Security("has_role('ROLE_NATURALISTE')")
+     */
+    public function bddAction(){
+        return $this->render('dashboard/extraction.html.twig');
     }
 
 }
