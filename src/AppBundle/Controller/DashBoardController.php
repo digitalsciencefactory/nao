fe6e8c1f4d31d0fbd44bd83e5efbaba16364f83d
Simon Lhoir <simon@lhoir.me>
2017-08-19 19:36:35 +0200

    #28 modification du menu en fonction du role, modification partie espace naturaliste du menu normal

--- src/AppBundle/Controller/DashBoardController.php
+++ src/AppBundle/Controller/DashBoardController.php
@@ -16,7 +16,6 @@ class DashBoardController extends Controller
         return $this->render('dashboard/accueil.html.twig');
     }
 
-
     /**
      * @Route("/dashboard/extraction-donnees", name="fn_dashboard_bdd")
      * @Security("has_role('ROLE_NATURALISTE')")
@@ -24,6 +23,5 @@ class DashBoardController extends Controller
     public function bddAction(){
         return $this->render('dashboard/extraction.html.twig');
     }
-
 
 }
