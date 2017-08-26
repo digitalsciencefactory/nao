5903deef400a86af2d8746485e09ac8d215d6148
Simon Lhoir <simon@lhoir.me>
2017-08-26 12:20:49 +0200

    bddAction to service

--- src/AppBundle/Controller/DashBoardController.php
+++ src/AppBundle/Controller/DashBoardController.php
@@ -1,5 +1,6 @@
 <?php
 namespace AppBundle\Controller;
+use AppBunble\Service\ExtractionService;
 use AppBundle\Entity\Observation;
 use AppBundle\Extraction\Extraction;
 use AppBundle\Form\Type\ExtractType;
@@ -25,45 +26,35 @@ class DashBoardController extends Controller
      * @Route("/dashboard/extraction-donnees")
      * @Security("has_role('ROLE_NATURALISTE')")
      */
-    public function bddAction(Request $request){
+    public function bddAction(Request $request, ExtractionService $extractionService){
         $extraction = new Extraction();
         $file = "";
         $form = $this->createForm(ExtractType::class, $extraction);
         $form->handleRequest($request);
+
         if($form->isSubmitted() && $form->isValid()) {
-            // on vérifie que les dates sont dans le bon sens
-            if($extraction->datefinApresDatedebut()){
-                // on récupère les observations validées et entre les 2 dates
-                $manager = $this
-                    ->getDoctrine()
-                    ->getManager();
-                $observationRepository = $manager->getRepository('AppBundle:Observation');
-                $observations = $observationRepository->extract($extraction->getDatedebut(), $extraction->getDatefin());
-                // si il y a au moins 1 observation
-                if($observations !== null && count($observations) !== 0){
 
-                    // crée le fichier et retourne le chemin du fichier
-                    $file = $this->createCsvFile($extraction, $observations);
+            try {
+                $observations = $extractionService->getObservationsDatees($extraction);
+                $file = $extractionService->generateCsv($extraction,$observations, $this->getParameter('downloads_dir'),$this->getParameter('entete_csv_extract'));
 
-                    // notice du téléchargement
-                    $request->getSession()->getFlashBag()->add('notice', 'La requête a retournée ' . count($observations) .' observation(s). Le téléchargement va commencer automatiquement.');
-                    // création de la réponse html
-                    $response = $this->render('dashboard/extraction.html.twig', array(
-                        'fichierExtract' => $file, // file est le string du chemin du fichier
-                        'form' => $form->createView(),
-                    ));
-                    // création du refresh dans le header pour déclencher le download du fichier
-                    $response->headers->set('Refresh', '2; url='.$this->generateUrl('fn_dashboard_extract', array('slug' => $file)));
-                    // envoi de la double réponse
-                    return $response;
-                } else {
-                    // ajout d'une notice comme quoi la requête n'a renvoyée aucun résultat
-                    $request->getSession()->getFlashBag()->add('notice', 'La requête n\'a retourné aucune observation.');
-                }
-            } else {
-                // on informe l'utilisateur du problème
-                $form->addError(new FormError("La date de début doit être antérieur à  la date de fin"));
+                $request->getSession()->getFlashBag()->add('notice', 'La requête a retournée ' . count($observations) .' observation(s). Le téléchargement va commencer automatiquement.');
+
+                // création de la réponse html
+                $response = $this->render('dashboard/extraction.html.twig', array(
+                    'fichierExtract' => $file, // file est le string du chemin du fichier
+                    'form' => $form->createView(),
+                ));
+
+                // création du refresh dans le header pour déclencher le download du fichier
+                $response->headers->set('Refresh', '2; url='.$this->generateUrl('fn_dashboard_extract', array('slug' => $file)));
+
+                // envoi de la double réponse
+                return $response;
+            } catch(\RuntimeException $re){
+                $request->getSession()->getFlashBag()->add('notice', $re->getMessage());
             }
+
         }
         return $this->render('dashboard/extraction.html.twig', array(
             'fichierExtract' => $file,
@@ -78,7 +69,7 @@ class DashBoardController extends Controller
      */
     public function fileAction($slug)
     {
-        $path = $this->get('kernel')->getRootDir() . '/../web/downloads/';
+        $path = $this->getParameter("downloads_dir");
         return $this->file($path.$slug);
     }
     /**
@@ -264,34 +255,7 @@ class DashBoardController extends Controller
         return $users;
     }
 
-    /**
-     * @param $extraction
-     * @param $observations
-     * @return string
-     */
-    protected function createCsvFile($extraction, $observations)
-    {
-        $path = $this->getParameter('downloads_dir');
-        // création d'un fichier csv
-        $now = new \DateTime();
-        $file =
-            "FNAT_exctract_"
-            . $now->format("Ymd_His_")
-            . $extraction->getDatedebut()->format("Ymd")
-            . "_"
-            . $extraction->getDatefin()->format("Ymd")
-            . ".csv";
-        // ouverture en écriture
-        $handle = fopen($path . $file, "w");
-        // écriture de l'entête
-        $entete = array("date observation", "lb nom", "nom vern", "nom vern eng", "latitude", "longitude", "commentaire observateur", "commentaire naturaliste");
-        fputcsv($handle, $entete);
-        // écriture de chaque ligne
-        foreach ($observations as $observation) {
-            fputcsv($handle, $observation->toArray());
-        }
-        // fermeture du fichier
-        fclose($handle);
-        return $file;
-    }
 }
+
+
+
